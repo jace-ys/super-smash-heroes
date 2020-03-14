0e11caad62dc1e75ed76132e837d056ba08fffc6
jace-ys <jaceys.tan@gmail.com>
2020-03-14 16:49:55 +0000

    Add migrations for superheroes table

---
+++ databases/postgres/migrations/20200314144140_seed_superheroes_table.down.sql
@@ -0,0 +1,4 @@
+DELETE FROM superheroes WHERE full_name = 'Oliver Queen';
+DELETE FROM superheroes WHERE full_name = 'Kara Zor-El';
+DELETE FROM superheroes WHERE full_name = 'Barry Allen';
+DELETE FROM superheroes WHERE full_name = 'Peter Parker';
---
+++ databases/postgres/migrations/20200314144140_seed_superheroes_table.up.sql
@@ -0,0 +1,23 @@
+INSERT INTO superheroes (full_name, alter_ego, image_url, intelligence, strength, speed, durability, power, combat) VALUES (
+  'Oliver Queen', 'Green Arrow',
+  'https://www.superherodb.com/pictures2/portraits/10/100/696.jpg',
+  81, 12, 35, 28, 39, 90
+);
+
+INSERT INTO superheroes (full_name, alter_ego, image_url, intelligence, strength, speed, durability, power, combat) VALUES (
+  'Kara Zor-El', 'Supergirl',
+  'https://www.superherodb.com/pictures2/portraits/10/100/790.jpg',
+  94, 100, 100, 100, 100, 75
+);
+
+INSERT INTO superheroes (full_name, alter_ego, image_url, intelligence, strength, speed, durability, power, combat) VALUES (
+  'Barry Allen', 'Flash II',
+  'https://www.superherodb.com/pictures2/portraits/10/100/892.jpg',
+  88, 48, 100, 60, 100, 60
+);
+
+INSERT INTO superheroes (full_name, alter_ego, image_url, intelligence, strength, speed, durability, power, combat) VALUES (
+  'Peter Parker', 'Spider-Man',
+  'https://www.superherodb.com/pictures2/portraits/10/100/133.jpg',
+  90, 55, 67, 75, 74, 85
+);
